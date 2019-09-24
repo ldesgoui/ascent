e8405f56a4572f7e5b90d65c1e2b0f27529077bd
ldesgoui <ldesgoui@gmail.com>
2019-09-24 10:09:07 +0200

    netlify as an env var

--- shell.nix
+++ shell.nix
@@ -10,6 +10,7 @@ mkShell {
   ];
 
   shellHook = ''
-     alias vi.nuxt="vi \$(find pages components layouts store -type f) *.config.js static/admin/config.yml"
+    export NETLIFY_SITE_ID="bfc4272c-f599-43b2-8b33-35c220201104"
+    alias vi.nuxt="vi \$(find pages components layouts store -type f) *.config.js static/admin/config.yml"
   '';
 }
