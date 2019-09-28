1a4588a6503c1c63ce2526a3cc1219b39908e6de
ldesgoui <ldesgoui@gmail.com>
2019-09-28 20:58:21 +0200

    vertical & horizontal

--- pages/overlays/sponsors.vue
+++ pages/overlays/sponsors.vue
@@ -27,6 +27,10 @@ export default {
   async mounted() {
     let cycle = 30000
 
+    if (location.search.includes("vertical")) {
+      this.$el.classList.add("flex-col")
+    }
+
     if (location.search.includes("debug")) {
       cycle = 5000
       this.$el.classList.add("bg-red-500")
@@ -50,6 +54,14 @@ export default {
 </script>
 
 <style>
+.flex-col :nth-child(1) {
+  order: 2;
+}
+
+.flex-col .w-1\/3 {
+  width: 100%;
+}
+
 img,
 #fill {
   filter: drop-shadow(0 0 1vw black);
@@ -70,8 +82,9 @@ img,
 
 .animation-high {
   & #stroke path {
+    stroke: transparent;
     stroke-dashoffset: 0 !important;
-    transition: stroke-dashoffset 3s;
+    transition: stroke-dashoffset 3s, stroke 500ms 3s;
   }
 
   & img,
