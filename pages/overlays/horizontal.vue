094bc669bedf0511436f1cf84f55882d6d2419c4
ldesgoui <ldesgoui@gmail.com>
2019-09-28 15:30:14 +0200

    optimize + add link

--- pages/overlays/horizontal.vue
+++ pages/overlays/horizontal.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="flex flex-wrap items-center bg-red-500 animation-low">
+  <div class="flex flex-wrap items-center">
     <img class="w-1/3" src="~/assets/img/sponsors/marketplace.png" />
-    <div class="w-1/3" v-html="require('~/assets/img/logo.svg?include')" />
+    <div class="w-1/3">
+      <div
+        id="fill"
+        class="absolute w-1/3"
+        v-html="require('~/assets/img/logo-and-url.svg?include')"
+      />
+      <div
+        id="stroke"
+        v-html="require('~/assets/img/logo-and-url.svg?include')"
+      />
+    </div>
     <img class="w-1/3" src="~/assets/img/sponsors/kovaak.png" />
   </div>
 </template>
@@ -15,6 +25,13 @@ export default {
   layout: "nothing",
 
   async mounted() {
+    let cycle = 30000
+
+    if (location.search.includes("debug")) {
+      cycle = 5000
+      this.$el.classList.add("bg-red-500")
+    }
+
     document.querySelectorAll("svg > path").forEach(el => {
       const len = el.getTotalLength()
 
@@ -23,106 +40,44 @@ export default {
     })
 
     while (true) {
-      await sleep(5000)
-      this.$el.classList.add("animation-rise")
-      this.$el.classList.remove("animation-low")
-      await sleep(5000)
+      await sleep(cycle)
       this.$el.classList.add("animation-high")
-      this.$el.classList.remove("animation-rise")
-      await sleep(5000)
-      this.$el.classList.add("animation-fall")
+      await sleep(cycle)
       this.$el.classList.remove("animation-high")
-      await sleep(5000)
-      this.$el.classList.add("animation-low")
-      this.$el.classList.remove("animation-fall")
     }
   },
 }
 </script>
 
 <style>
-svg,
-img {
-  filter: drop-shadow(0 0 2vw black);
+img,
+#fill {
+  filter: drop-shadow(0 0 1vw black);
+  opacity: 0;
+  transition: opacity 500ms;
 }
 
-svg path {
+#fill svg {
+  fill: white;
+}
+
+#stroke path {
+  fill: transparent;
   stroke: white;
   stroke-width: 4;
+  transition: stroke-dashoffset 3s 500ms;
 }
 
-.animation-low,
-.animation-rise {
-  & svg path {
-    fill: transparent;
+.animation-high {
+  & #stroke path {
+    stroke-dashoffset: 0 !important;
+    transition: stroke-dashoffset 3s;
   }
 
-  & img {
-    opacity: 0;
-  }
-}
-
-.animation-rise {
-  & svg path {
-    animation: 3s ease forwards stroke-in, 500ms ease forwards 2s fill-in;
-  }
-
-  & img {
-    animation: 500ms ease forwards 2s opacity-in;
-  }
-}
-
-@keyframes stroke-in {
-  to {
-    stroke-dashoffset: 0;
-  }
-}
-
-@keyframes fill-in {
-  to {
-    fill: white;
-    stroke: transparent;
-  }
-}
-
-@keyframes opacity-in {
-  to {
+  & img,
+  & #fill {
     opacity: 1;
-  }
-}
-
-.animation-high,
-.animation-fall {
-  & svg path {
-    fill: white;
-    stroke: transparent;
-  }
-
-  & img {
-    opacity: 1;
-  }
-}
-
-.animation-fall {
-  & svg path {
-    animation: 500ms forwards fill-out;
-  }
-
-  & img {
-    animation: 500ms forwards opacity-out;
-  }
-}
-
-@keyframes fill-out {
-  to {
-    fill: transparent;
-    stroke: white;
-  }
-}
-
-@keyframes opacity-out {
-  to {
-    opacity: 0;
+    transition: opacity 500ms 3s;
   }
 }
 </style>
