52517e4848d702ff95cb326bd3e4b060fef3b1b8
ldesgoui <ldesgoui@ldesgoui.xyz>
2019-11-14 21:48:15 +0100

    boo

--- shell.nix
+++ shell.nix
@@ -11,6 +11,5 @@ mkShell {
 
   shellHook = ''
     export NETLIFY_SITE_ID="7d8993ef-9803-468a-8062-48234c8ec1a6"
-    alias vi.nuxt="vi \$(find pages components layouts store -type f) *.config.js static/admin/config.yml"
   '';
 }
